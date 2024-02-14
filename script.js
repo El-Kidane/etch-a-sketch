f6f6dcd6f58e6968597d2df15a0e6f237be5edf9
El-Kidane <159628429+El-Kidane@users.noreply.github.com>
2024-02-14 18:52:50 +0100

    Adaptive grid size. New grid button. Not limited to 100 yet.

--- script.js
+++ script.js
@@ -1,22 +1,62 @@
 // script.js
+const container = document.getElementById('container');
+const body = document.querySelector('body');
+let gridRow;
 
-window.onload = function () {
-    let divRow = 16,
-        divCol = divRow,
-        div,
-        container = document.getElementById('container'),
-        fragment = document.createDocumentFragment();
-    
-    // rows
-    for (let i = 0; i < divRow; ++i) {
+function createHeader() {
+    let
+        header,
+        headerTxt,
+        headerBtn,
+        headerFragment = document.createDocumentFragment();
 
-        // columns
-        for (let j = 0; j < divCol; ++j) {
-            div = document.createElement('div');
-            div.className = 'grid-cell';
-            fragment.appendChild(div);
+    header = document.createElement('div');
+    header.id = 'header';
+    headerTxt = document.createElement('span');
+    headerTxt.className = 'header-txt';
+    headerTxt.textContent = 'You can create a new grid, max 100 per row'
+    headerBtn = document.createElement('button');
+    headerBtn.id = 'header-btn'
+    headerBtn.textContent = 'New grid'
+
+    headerBtn.addEventListener('click', createNewGrid);
+
+    headerFragment.appendChild(header);
+    header.appendChild(headerTxt);
+    header.appendChild(headerBtn);
+    body.insertBefore(headerFragment, container);
+}
+
+function createGrid(gridRow) {
+    let gridCellSize = 800 / gridRow;
+    let gridCol = gridRow,
+        gridCell,
+        cellWidth = 'width',
+        cellHeight = 'height',
+        cellFragment = document.createDocumentFragment();
+    console.log(gridCellSize);
+    // grid rows/cols
+    for (let i = 0; i < gridRow; ++i) {
+        for (let j = 0; j < gridCol; ++j) {
+
+            gridCell = document.createElement('div');
+            gridCell.className = 'grid-cell';
+            gridCell.style[cellHeight] = `${gridCellSize}px`;
+            gridCell.style[cellWidth] = `${gridCellSize}px`;
+            cellFragment.appendChild(gridCell);
         }
     }
-
-    container.appendChild(fragment);
+    console.log(gridCellSize);
+    //append all cells div to DOM only once, no reflow
+    container.appendChild(cellFragment);
 };
+
+function createNewGrid() {
+    gridRow = +prompt('New grid size?', 'Enter the number. 13 would return a grid of 13x13 squares');
+    document.querySelector('#container').innerHTML = "";
+    createGrid(gridRow);
+}
+
+window.onload = createHeader();
+window.onload = createGrid(16);
+
